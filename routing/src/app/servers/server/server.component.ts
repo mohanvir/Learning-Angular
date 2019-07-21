01b9803fea2a1abf1b8eb9865d4d203261ed1368
Mohanvir Gill <mohanvir.gill@powerschool.com>
2019-07-21 08:48:54 -0700

    feat: adding url query and fragments

---
+++ routing/src/app/servers/server/server.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+import { ServersService } from '../servers.service';
+import { ActivatedRoute, Params } from '@angular/router';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.css']
+})
+export class ServerComponent implements OnInit {
+  server: {id: number, name: string, status: string};
+  idNumber: number;
+
+  constructor(private serversService: ServersService, private route:ActivatedRoute) { }
+
+  ngOnInit() {
+    this.idNumber = this.route.snapshot.params['id'];
+
+    this.server = this.serversService.getServer( Number(this.idNumber));
+
+    this.route.params.subscribe((params:Params) =>{
+      this.server = this.serversService.getServer( Number(params['id']));
+
+    })
+  }
+
+}
